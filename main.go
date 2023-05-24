62af9855f0c957d38d53d06b83544ef622176dbe
LucasMonir <lucasmonir@hotmail.com>
2023-05-24 15:35:45 -0300

    updated code to handle shrooms!

--- main.go
+++ main.go
@@ -6,38 +6,38 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type album struct {
+type shroom struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
 
-var albums = []album{
+var shrooms = []shroom{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-func getAlbums(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, albums)
+func getShrooms(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, shrooms)
 }
 
-func postAlbums(c *gin.Context) {
-	var newAlbum album
+func postShrooms(c *gin.Context) {
+	var newShroom shroom
 
-	if err := c.BindJSON(&newAlbum); err != nil {
+	if err := c.BindJSON(&newShroom); err != nil {
 		return
 	}
 
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	shrooms = append(shrooms, newShroom)
+	c.IndentedJSON(http.StatusCreated, newShroom)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", getAlbums)
-	router.POST("/albums", postAlbums)
+	router.GET("/albums", getShrooms)
+	router.POST("/albums", postShrooms)
 
 	router.Run("localhost:4200")
 }
