6bbe2d3a334e2a54898a6d1b44ed26b4c8e87278
LucasMonir <lucasmonir@hotmail.com>
2023-05-24 19:11:40 -0300

    added new fields to shroom object and some mock values for development

--- main.go
+++ main.go
@@ -7,16 +7,19 @@ import (
 )
 
 type shroom struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+	ID          string `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Genus       string `json:"genus"`
+	Species     string `json:"species"`
+	Img         string `json:"img"`
 }
 
 var shrooms = []shroom{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "1", Title: "Amanita phalloides", Description: "Amanita phalloides, commonly known as the death cap, is a deadly poisonous basidiomycete fungus, one of many in the genus Amanita.", Img: "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcSjfmn3dnqwqbz-trVZPIF4nX9SIVKGJxa0wfVjQ1CJAx9-zUmHuEXmvMKdhfI4veqUrzFxce1MhFjEkLA"},
+	{ID: "4", Title: "Amanita phanterina", Description: "Amanita pantherina, also known as the panther cap, false blusher, and the panther amanita due to its similarity to the true blusher, is a species of fungus found in Europe and Western Asia", Img: "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcS2jPfF9E740FTgearAS0JLauc6sn_nPleiKl4Yg56krHxH5-K94dsnxx4xj8FEa8YdQxGhcKqUWsAFYz4"},
+	{ID: "3", Title: "Amanita muscaria", Description: "Amanita muscaria, commonly known as the fly agaric or fly amanita, is a basidiomycete of the genus Amanita. It is also a muscimol mushroom.", Img: "https://www.naturezadivina.com.br/media/amasty/blog/amanita-muscaria.jpg"},
+	{ID: "2", Title: "Amanita roseolamellata", Description: "This species is a readily recognisable Amanita with few collections. The habitat of this species is highly fragmented and in decline. Many of these areas are small patches of forest surrounded by urban or agricultural land, with recent (within the last few decades) severe decline in forest extent and increasing pressure for urban development.", Img: "http://www.amanitaceae.org/image/uploaded/r/roseolam139307_web.jpg"},
 }
 
 func getShrooms(c *gin.Context) {
