7851eb85094b6bb3e67349a864000907fa4ad50f
LucasMonir <lucasmonir@hotmail.com>
2023-06-25 22:53:31 -0300

    slight change to cors policy

--- main.go
+++ main.go
@@ -137,13 +137,32 @@ func initDb() {
 	}
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 
 	router := gin.Default()
 	initDb()
 
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000"}
+	router.Use(CORSMiddleware())
+
+	config.AllowOrigins = []string{"http://localhost:3000", "http://192.168.0.147:3000"}
 
 	router.Use(cors.New(config))
 	router.GET("/shrooms", getShrooms)
